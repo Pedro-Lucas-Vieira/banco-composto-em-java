7f8ea5a75d98ad47a30cdb753c720eebe024f048
Pedro-Lucas-Vieira <104370412+Pedro-Lucas-Vieira@users.noreply.github.com>
2022-04-25 11:37:06 -0300

    Add files via upload

---
+++ Conta.java
@@ -0,0 +1,39 @@
+public class Conta {
+   private double saldo;
+    int agencia = 42;
+    int numero;
+    Cliente titular; //cliente ´referencia a uma class
+    
+    
+  public void deposita(double valor) {
+	  this. saldo +=  valor ;
+   }
+
+
+  public boolean saca(double valor) {
+	  if (this.saldo >= valor) {
+		  this.saldo -= valor;
+		  return true;
+		  
+	  }else {
+		  return false;
+	  }
+		  
+	  }
+
+ public boolean transfere (double valor, Conta destino) {
+	  if(this.saldo >= valor) { 
+          this.saldo -= valor; 
+          destino.deposita(valor);
+          return true;
+      } else {
+          return false;
+      }
+	  
+	}
+ 
+ public double getSaldo() { 
+     return this.saldo;
+	   
+  }
+}// O THIS REPRESENTA O CLASS CONTA 	
